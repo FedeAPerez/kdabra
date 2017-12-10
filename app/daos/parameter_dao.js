2c93fa418d12f985f0df017a361d9bdf022ff946
Federico Perez <fedeaperez@hotmail.com.ar>
2017-12-10 14:32:31 -0300

    agregado de parametros

--- app/daos/parameter_dao.js
+++ app/daos/parameter_dao.js
@@ -1,6 +1,17 @@
 // routes/parameter_dao.js
 
 module.exports = {
+	saveParameter
+	 : function(db, parameter, callback) {
+		db.collection('Parameters').insert(parameter, function(err, result) {
+			 if (err) { 
+		        console.error('An error has occurred saving parameter'); 
+		        return callback(parameter, false);
+		      } else {
+		        return callback(parameter, true);
+		      }
+		});
+	},
 	getParameters 
 	: function(db, callback) {
 		db.collection('Parameters').find().toArray(function(err, result) {
--- app/routes/parameter_routes.js
+++ app/routes/parameter_routes.js
@@ -17,4 +17,38 @@ module.exports = function(app, db) {
   		}
   	);
 
+  	app.post('/parameters', [
+			check('page_id')
+	    	.isNumeric().withMessage('page_id must be numeric'),
+	    	check('value')
+	    	.isAlphanumeric().withMessage('value must be alphanumeric')
+		],
+
+		(req, res) => {
+	    	// Saves a new Parameter
+
+	    	var errors = validationResult(req);
+
+			if (!errors.isEmpty()) {
+			    res.status(422).send({ operation: 'Create Parameter', errors: errors.mapped() });
+	    		return;
+	    		
+			}
+			else {
+			    // There are no validation errors
+			   	var parameter = {
+	    			page_id 	 	 : req.body.page_id,
+	    			parameter_id	 : req.body.parameter_id,
+	    			value 	 	 	 : req.body.value
+	    		}
+
+				parameterDao.saveUser(db, parameter, function(parameterSaved, result){
+	    			res.status(200).send( {operation : 'Create Parameter', result : result, item : parameterSaved});
+	    			return;
+	    		});
+
+			}
+  		}
+  	);
+
 };
